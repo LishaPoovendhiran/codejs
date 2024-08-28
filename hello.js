0dd65606646c34016b7ca671f6fc0b7322dadd73
Lisha Poovendhiran <lishapoovendhiran@gmail.com>
2024-08-28 00:15:11 -0700

    challenge1

---
+++ hello.js
@@ -0,0 +1,9 @@
+// console.log("Hello World!")
+arr1 = [10 , 65 , 85 , 35 , 45]
+console.log(arr1)
+var a = arr1.pop()
+console.log(a) 
+arr1.push(23)
+console.log(arr1)
+arr1.shift()
+console.log(arr1)
