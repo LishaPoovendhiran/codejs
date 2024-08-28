d7e44ec1f939675655b3c9ce2cd9cdb527909901
Lisha Poovendhiran <lishapoovendhiran@gmail.com>
2024-08-28 01:51:57 -0700

    grade task

---
+++ grade.js
@@ -0,0 +1,14 @@
+var studentscore = 67
+if(studentscore>=90){
+    console.log("Grade A")
+}
+else if(studentscore >=70 && studentscore<=89){
+    console.log("Grade B")
+
+}
+else if(studentscore>=50 && studentscore<=69){
+    console.log("Grade C")
+}
+else{
+    console.log("Student Failed!")
+}
